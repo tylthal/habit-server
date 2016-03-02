c8091f2491a2875559b61699182692b6ad5bc291
Tyler Thalman <tyler.thalman@gmail.com>
2016-03-02 15:09:28 -0700

    Getting basic express functionality with typescript working

---
+++ app.ts
@@ -0,0 +1,11 @@
+import * as express from 'express';
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3001, function () {
+  console.log('Example app listening on port 3001!');
+});
